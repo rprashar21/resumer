293488af0cf0d0dd517af4301c233b7b54c4ae91
Rohit Prashar <rohitprashar712@gmail.com>
2025-04-13 07:33:22 +0100

    initial setup

---
+++ smart-resumer/src/main/java/resumer/smart_resumer/SmartResumerApplication.java
@@ -0,0 +1,13 @@
+package resumer.smart_resumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartResumerApplication.class, args);
+	}
+
+}
---
+++ smart-resumer/src/test/java/resumer/smart_resumer/SmartResumerApplicationTests.java
@@ -0,0 +1,13 @@
+package resumer.smart_resumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartResumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
